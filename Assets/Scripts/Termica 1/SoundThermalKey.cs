8af805d5dca465aa3e7dc7d53807d5e13b559dca
agusquindi7 <agustinquindimiltraduccion@gmail.com>
2024-09-26 18:42:19 -0300

    Commit 26-9-24
    Observer con IInteractuable completamente aplicado
    
    P.D: odio GitHub

--- Assets/Scripts/Termica 1/SoundThermalKey.cs
+++ Assets/Scripts/Termica 1/SoundThermalKey.cs
@@ -1,26 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class SoundThermalKey : MonoBehaviour, IObserver
 {
     public AudioClip sfx;
     [SerializeField] AudioSource aSource;
     IObservable _obs;
-
     private void Awake()
     {
-        _obs = GetComponent<IObservable>();
+        _obs = GetComponentInParent<IObservable>();
         if (_obs != null)
             _obs.Subscribe(this);
     }
-
     public void Notify(bool isOnButton)
     {
-        aSource.PlayOneShot(sfx,1f);
+        aSource.PlayOneShot(sfx, 1f);
     }
-
-    private void OnDestroy()
+    private void OnDisable()
     {
         _obs.Unsubscribe(this);
     }
--- Assets/Scripts/Termica 1/UIManager.cs
+++ Assets/Scripts/Termica 1/UIManager.cs
@@ -1,33 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class UIManager : MonoBehaviour , IObserver
+public class UIManager : MonoBehaviour, IObserver
 {
     public GameObject text;
     IObservable _obs;
-
     private void Start()
     {
         _obs = GetComponentInParent<IObservable>();
         if (_obs != null)
             _obs.Subscribe(this);
     }
-
     public void Notify(bool isOnButton)
     {
-        if (isOnButton) 
-        {
-            text.SetActive (true);
-        }
-        else
-        {
-            text.SetActive(false);
-        }
+        StartCoroutine(TextSeconds());
     }
-
     private void OnDestroy()
     {
         _obs.Unsubscribe(this);
+    }
+    IEnumerator TextSeconds()
+    {
+        text.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        text.SetActive(false);
     }
 }
