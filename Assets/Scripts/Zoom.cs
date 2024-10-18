4da2b987c8ef70ec9a839334cc15af59ef5aab08
agusquindi7 <agustinquindimiltraduccion@gmail.com>
2024-10-18 16:11:30 -0300

    18-10 clase

--- Assets/Scripts/Zoom.cs
+++ Assets/Scripts/Zoom.cs
@@ -7,13 +7,13 @@ public class Zoom : MonoBehaviour
     public Material zoomMat;
     float zoom = 0;
     [SerializeField, Range (0f,1f)] float zoomSpeed;
-    [SerializeField] KeyCode myCannonKey;
+    //[SerializeField] KeyCode myCannonKey;
  
     private void Update()
     {
 
 
-        if(Input.GetKey(myCannonKey))
+        if(Input.GetMouseButton(1))
         {
             zoom += (Time.time * zoomSpeed);
             zoom = Mathf.Clamp(zoom,0,0.5f);
