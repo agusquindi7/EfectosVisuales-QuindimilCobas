76b1c164d6a53185cb4df93a1142899ea5923fbd
LoJaxMac10 <maragustin2015@gmail.com>
2025-03-05 02:01:44 -0300

    commit PreFinal
    -Arreglos de script de lluvia y Win Condition
    -Decoracion Nivel 3 Chelo

--- Assets/Art/Chelo Nuevos VFX y Shaders/Rain VFx/ActiveRain.cs
+++ Assets/Art/Chelo Nuevos VFX y Shaders/Rain VFx/ActiveRain.cs
@@ -7,17 +7,26 @@ public class ActiveRain : MonoBehaviour
 {
     public VisualEffect vfxRAIN;
     public GameObject objective;
+    public GameObject triggerRain;
 
-    private void Start()
+    //private void Start()
+    private void Awake()
     {
+        //gameObject.SetActive(false);
+        //vfxRAIN.gameObject.SetActive(true);
         vfxRAIN.Stop();
+        triggerRain.SetActive(false);
+
     }
 
     private void Update()
     {
         if (!objective.activeInHierarchy)
         {
+            Debug.Log("el objetivo murio, prendiendo");
+            //vfxRAIN.gameObject.SetActive(true);
             vfxRAIN.Play();
+            triggerRain.SetActive(true);
         }
     }
 }
